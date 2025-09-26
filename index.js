7d8e7f267849c5f0d18fcadb8747a5e557eebab4
sumithra <sumithra@123gmail.com>
2025-09-26 14:49:17 +0530

    change the git code in vs code

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("first repositary")
+// Get the button and heading elements
+const button = document.getElementById('changeBtn');
+const heading = document.getElementById('greeting');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+  heading.textContent = "Hello, JavaScript is working!";
+  heading.style.color = "blue";
+});
